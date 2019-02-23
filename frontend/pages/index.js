3f6441a4dae6177ebf006bca049c67392d73ddda
chinedu onyeoka <chinedu.onyeoka@gmail.com>
2019-02-23 12:57:30 +0100

    first commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+
+
+const Home = props =>(
+    <div>
+        <p> hey baba</p>
+        <Link href="/sell">
+        <a>sellla!</a>
+        </Link>
+    </div>
+)
+
+export default Home;
